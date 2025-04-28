a716a2b3c05a4c79675011ac9d2ec9a6465f8bac
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-04-28 10:50:52 +0200

    fix: main.py

--- main.py
+++ main.py
@@ -1,55 +1,49 @@
 from colorama import Fore, Style
 from sentinel.utils import print_logo
 from simple_term_menu import TerminalMenu
-from sentinel.modules import IPGlobeTracker, SysInsider, PortBlitz, MACPhantom, KeyboardHandler
-from pynput import keyboard
-print(Fore.GREEN)
+from sentinel.modules import IPGlobeTracker, SysInsider, PortBlitz, MACPhantom
 
 def main():
-       keyboard_handler = KeyboardHandler()
-       # Avvia il listener
-       listener = keyboard.Listener(on_press=keyboard_handler.on_press_exit)
-       listener.start()
+    print_logo()
 
-       print_logo()
+    while True:
+        options = [
+            "[1] PortBlitz",
+            "[2] SysInsider",
+            "[3] IP GlobeTracker",
+            "[4] PacketHound",
+            "[5] MAC Phantom",
+            "[6] CVE Hunter",
+            "[esc] Exit"  # Aggiunta opzione esplicita per uscire
+        ]
 
-       while not keyboard_handler.should_exit:
-              options = [
-              "[1] PortBlitz",
-              "[2] SysInsider",
-              "[3] IP GlobeTracker",
-              "[4] PacketHound",
-              "[5] MAC Phantom",
-              "[6] CVE Hunter"
-              ]
+        terminal_menu = TerminalMenu(
+            options,
+            menu_cursor=">",
+            menu_cursor_style=("fg_green", "bold"),
+            menu_highlight_style=("standout",)
+        )
 
-              terminal_menu = TerminalMenu(
-              options,
-              menu_cursor=">",
-              menu_cursor_style=("fg_green", "bold"),
-              menu_highlight_style=("standout",)
-              )
+        choice = terminal_menu.show()
+        
+        # Gestione uscita con ESC o selezione "Exit"
+        if choice is None or (choice == len(options) - 1):
+            print(f'\n{Fore.GREEN}Thanks for using Sentinel, hope to see you soon!{Style.RESET_ALL}')
+            break
 
-              choice = terminal_menu.show()
-              
-              if choice == 0:
-                     port_blitz = PortBlitz()
-                     port_blitz.port_blitz_manager()
-              elif choice == 1:
-                     sys_insider = SysInsider() 
-                     sys_insider.print_system_info()
-              elif choice == 2:
-                     globe_tracker = IPGlobeTracker()
-                     globe_tracker.ip_globetracker_manager()
-              elif choice == 3:
-                     print('scelta 4 - PacketHound')
-              elif choice == 4:
-                     mac_phantom = MACPhantom()
-                     mac_phantom.mac_phantom_manager()
-              
-
-       listener.stop()  # Ferma correttamente il listener
-       print(f'{Fore.GREEN}Thanks for using Sentinel, hope to see you soon!{Style.RESET_ALL}')
+        # Gestione scelte
+        if choice == 0:
+            PortBlitz().port_blitz_manager()
+        elif choice == 1:
+            SysInsider().print_system_info()
+        elif choice == 2:
+            IPGlobeTracker().ip_globetracker_manager()
+        elif choice == 3:
+            print('PacketHound (funzionalità non implementata)')
+        elif choice == 4:
+            MACPhantom().mac_phantom_manager()
+        elif choice == 5:
+            print('CVE Hunter (funzionalità non implementata)')
 
 if __name__ == "__main__":
-       main()
+    main()
