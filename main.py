6b978f7bd48ea3f4929f498747cdbe539ab6f6c3
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-10 18:31:31 +0200

    remove mac spoofer

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
+#!/usr/bin/env python3
 from colorama import Fore, Style
 from sentinel.utils import print_welcome_message
 from simple_term_menu import TerminalMenu
-from sentinel.modules import IPGlobeTracker, SysInsider, PortBlitz, MACSpoofer, SQLInjectionScanner
+from sentinel.modules import IPGlobeTracker, SysInsider, PortBlitz, SQLInjectionScanner
 
 def main():
     print_welcome_message()
-
+    
     while True:
         options = [
             "[1] - PortBlitz",
             "[2] - SysInsider",
             "[3] - IP GlobeTracker",
-            "[4] - PacketHound",
+            "[4] - MAC Spoofer",      # Aggiunto MAC Spoofer al menu
             "[5] - SQL Injector",
+            "[6] - PacketHound",      # Spostato in posizione 6
             "",
             "[x] Exit"
         ]
-
+        
         terminal_menu = TerminalMenu(
             options,
             skip_empty_entries=True,
@@ -24,14 +26,14 @@ def main():
             menu_cursor_style=("fg_red", "bold"),
             menu_highlight_style=("standout",)
         )
-
+        
         choice = terminal_menu.show()
-
+        
         # Gestione uscita con ESC o selezione "Exit"
         if choice is None or (choice == len(options) - 1):
             print(f'\n{Fore.BLUE}Thanks for using Sentinel, hope to see you soon!{Style.RESET_ALL}')
             break
-
+        
         # Gestione scelte
         if choice == 0:
             PortBlitz().port_blitz_manager()
@@ -40,11 +42,11 @@ def main():
         elif choice == 2:
             IPGlobeTracker().ip_globetracker_manager()
         elif choice == 3:
-            print('PacketHound (funzionalità non implementata)')
+            print('Pippo')
         elif choice == 4:
             SQLInjectionScanner().sql_injector_manager()
         elif choice == 5:
-            print('CVE Hunter (funzionalità non implementata)')
+            print('PacketHound (funzionalità non implementata)')
 
 if __name__ == "__main__":
     main()
--- sentinel/modules/__init__.py
+++ sentinel/modules/__init__.py
@@ -1,7 +1,6 @@
 from sentinel.modules.ip_globetracker import IPGlobeTracker
 from sentinel.modules.sys_insider import  SysInsider
 from sentinel.modules.port_blitz import PortBlitz
-from sentinel.modules.mac_phantom import MACSpoofer
 from sentinel.modules.sql_injector import SQLInjectionScanner
 
-__all__ = ["IPGlobeTracker", "SysInsider", "PortBlitz", "MACSpoofer", "SQLInjectionScanner"]  # Definisce cosa è esportato con 'from sentinel import *'
+__all__ = ["IPGlobeTracker", "SysInsider", "PortBlitz", "SQLInjectionScanner"]  # Definisce cosa è esportato con 'from sentinel import *'
