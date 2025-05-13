42dd301bc9beb44b03d3029749440c953d8b457b
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-05-13 10:16:11 +0200

    remove: sqlinjection tester

--- sentinel/modules/__init__.py
+++ sentinel/modules/__init__.py
@@ -1,6 +1,5 @@
 from sentinel.modules.ip_globetracker import IPGlobeTracker
 from sentinel.modules.sys_insider import  SysInsider
 from sentinel.modules.port_blitz import PortBlitz
-from sentinel.modules.sql_injector import SQLInjectionTester
 
-__all__ = ["IPGlobeTracker", "SysInsider", "PortBlitz", "SQLInjectionTester"]  # Definisce cosa è esportato con 'from sentinel import *'
+__all__ = ["IPGlobeTracker", "SysInsider", "PortBlitz"]  # Definisce cosa è esportato con 'from sentinel import *'
