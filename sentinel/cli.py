bc749dc9271a8bcdb15388fa6e34c290ed07d2d0
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-09-03 19:42:38 +0200

    fix: messages

--- sentinel/cli.py
+++ sentinel/cli.py
@@ -2,33 +2,30 @@ import click
 import subprocess
 import os
 import sys
+from colorama import Fore, Style
 
 @click.group()
 def cli():
-    """Sentinel CLI Tool"""
     pass
 
 @cli.command()
 def start():
-    """Avvia l'applicazione Sentinel"""
+    """start Sentinel"""
     # Trova il percorso assoluto
     base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     venv_python = os.path.join(base_dir, "sentinel-venv", "bin", "python")
     main_script = os.path.join(base_dir, "main.py")
     
     if not os.path.exists(venv_python):
-        click.echo("ERRORE: Virtual environment non trovato!")
+        click.echo(f"{Fore.RED}[X] error: Virtual environment non trovato!{Style.RESET_ALL}")
         sys.exit(1)
     
     click.echo("Starting Sentinel...")
     try:
         subprocess.run(["sudo", "-E", venv_python, main_script], check=True)
     except subprocess.CalledProcessError as e:
-        click.echo(f"Errore durante l'avvio: {e}")
+        click.echo(f"{Fore.RED}[X] Error during startup: {e}{Style.RESET_ALL}")
         sys.exit(1)
     except FileNotFoundError:
-        click.echo("sudo non trovato o permessi insufficienti")
+        click.echo(f"{Fore.RED}[X] sudo not found or insufficient permissions.{Style.RESET_ALL}")
         sys.exit(1)
-
-if __name__ == "__main__":
-    cli()
