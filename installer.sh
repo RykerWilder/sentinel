d4c66617d2030bb7a5e5c9ca1581306fa558b3b8
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-09-04 09:40:30 +0200

    add: installer.sh

---
+++ installer.sh
@@ -0,0 +1,50 @@
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m'
+
+# error handler
+error_exit() {
+    echo -e "${RED}Error: $1${NC}" >&2
+    exit 1
+}
+
+# check dependencies
+check_dependencies() {
+    echo -e "${YELLOW}Checking dependencies...${NC}"
+    
+    if ! command -v git &> /dev/null; then
+        error_exit "Git is not installed. Please install it before proceeding."
+    fi
+    
+    if ! command -v python3 &> /dev/null; then
+        error_exit "Python3 is ont installed. Please install it before proceeding."
+    fi
+}
+
+clone_project() {
+    # cloning
+    git clone https://github.com/RykerWilder/sentinel || error_exit "Clone failed"
+
+    cd sentinel
+}
+
+# python3 dependencies
+install_python_deps() {
+    echo -e "${YELLOW}Installing python3 dependencies...${NC}"
+    
+    python3 -m venv sentinel-venv || error_exit "Virtual environment creation failed."
+    
+    source sentinel-venv/bin/activate
+    pip install --upgrade pip
+    pip install -r requirements.txt || error_exit "Requirements installation failed."
+}
+
+# MAIN
+main() {
+    check_dependencies
+    clone_project
+    install_python_deps
+
+    echo -e "${GREEN} Sentinel installed. Please run on terminal 'sentinel start'."
+}
