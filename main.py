56b4c449284a8d777e5f7177ee18a90c1aa639c0
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-10-01 20:29:30 +0200

    fix: sentinel tree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from sentinel.utils import print_welcome_message, exit
-from sentinel.modules import IPGlobeTracker, SystemInfo, NetworkScanner, PacketSniffer, P2P
+from modules.utils import print_welcome_message, exit
+from modules import IPGlobeTracker, SystemInfo, NetworkScanner, PacketSniffer, P2P
 from colorama import Fore, Style
 import signal
 
