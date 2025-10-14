08ecd21ffc3fc3b249670d3b8ef323c355c2b2cc
RykerWilder <fabiomasciarelliwd@gmail.com>
2025-10-14 21:48:24 +0200

    fix: messages

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ def main():
         try:
             choice = int(input(f"{Fore.BLUE}[?]{Style.RESET_ALL} Insert your choice => "))
         except (ValueError, IndexError):
-            print(f"{Fore.RED}[ERROR] Invalid input.{Style.RESET_ALL}")
+            print(f"{Fore.RED}[X] Invalid input.{Style.RESET_ALL}")
 
         
         
